60d918bc4836b341ec92d23541815c138d967915
Ugochukwu Ezekwem <the_zex@yahoo.com>
2025-05-23 15:00:18 +0100

    cleanup 2025

--- source/_partials/layout/footer.blade.php
+++ source/_partials/layout/footer.blade.php
@@ -63,7 +63,7 @@
   </div>
   <div class="flex flex-col-reverse justify-between pt-5 pb-10 border-t lg:flex-row">
     <p class="text-sm text-gray-400">
-      © Copyright 2024 Zedia Kaizen. All rights reserved.
+      © Copyright 2025 Zedia Kaizen. All rights reserved.
     </p>
     <ul class="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
       <li>
